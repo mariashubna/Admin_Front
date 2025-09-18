47c0d070698343ba6e676421caf00b6240083dc4
Mariia <mariashubna@gmail.com>
2025-09-18 18:13:28 +0200

    bugs

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,8 +1,8 @@
 import { Route, Routes } from "react-router-dom";
-import HomePage from "@pages/home/HomePage";
+// import HomePage from "@pages/home/HomePage";
 import NotFoundPage from "@pages/not-found/NotFoundPage.jsx";
 import { Layout } from "@components/layout/Layout.jsx";
-import PrivateRoute from "@components/PrivateRoute.jsx";
+// import PrivateRoute from "@components/PrivateRoute.jsx";
 import AuthPage from "../pages/authPage/AuthPage.jsx";
 import UsersPage from "../pages/users/UsersPage.jsx";
 import DashboardPage from "../pages/dashboard/DashboardPage.jsx";
